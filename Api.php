332719b78dfb64f97a22db09fae2a75a2473862d
lera <chernv91@gmail.com>
2019-04-19 11:08:37 +0300

    update

--- Api.php
+++ Api.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'UserApi.php';
 
 class Api
@@ -20,21 +19,17 @@ class Api
 
     public function __construct()
     {
-        header("Access-Control-Allow-Orgin: http://charlie.rarus-crimea.ru/final_task");
-        //header("Access-Control-Allow-Methods: *");
+        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Methods: *");
         $this->requestUri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
         if ($this->requestUri[0] !== 'api' || !in_array($this->requestUri[1], $this->routers, true)) {
             header('HTTP/1.0 400 Bad Request');
             echo json_encode(['code' => '400', 'message' => 'API Not Found']);
             exit();
         } else {
-            require_once 'config.php';
-            global $config;
-            $this->config = $config;
-            unset($config);
+            $this->config = json_decode(file_get_contents('config.txt'), true);
             //Раскоментить только после того как в скрипте везде будет джейсон!
             //header("Content-Type: application/json");
-
             $this->requestMethod = $_SERVER['REQUEST_METHOD'];
             $this->requestParams = $_REQUEST;
             $this->action = $this->getAction();
@@ -42,7 +37,6 @@ class Api
             $this->loyaltyProgram = array_search(true, $this->config['loyalty_program'], true);
             $this->cardNumberType = array_search(true, $this->config['card_number_type'], true);
             $this->sumBonus = array_reverse($this->config['sum_bonus'], true);
-
             $obj_name = explode('_', substr($this->requestUri[1], 0, -1));
             foreach ($obj_name as &$part) {
                 $part = ucfirst($part);
@@ -50,7 +44,6 @@ class Api
             unset($part);
             $obj_name = implode($obj_name);
             $this->objName = $obj_name . 'Api';
-
             if ('CalculatorApi' === $this->objName) {
                 $param = $this->requestUri[2];
                 $this->objMethodName = $param === 'bonuses' ? 'getBonuses' : 'getMaxPossibleSum';
@@ -60,11 +53,9 @@ class Api
                 } else {
                     $this->objMethodName = $this->action . $obj_name;
                 }
-
             }
             file_put_contents('20.txt', $this->objName . '  ' . $this->objMethodName, FILE_APPEND);
         }
-
     }
 
     public function connectDb()
@@ -77,7 +68,6 @@ class Api
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
-
         $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
         try {
             $this->db = new PDO($dsn, $user, $password, $options);
@@ -89,7 +79,6 @@ class Api
     public function getAction()
     {
         $action = null;
-
         switch ($this->requestMethod) {
             case 'GET':
                 $action = 'get';
@@ -104,7 +93,6 @@ class Api
                 $action = 'create';
                 break;
         }
-
         return $action;
     }
 }
